393377102dc25755ad131ada014f779ae63a7786
rv022 <rmv6@njit.edu>
2025-09-30 22:54:14 -0400

    Exercise 3 2.0

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const { v4: uuidv4 } = require('uuid');
 
 // Middleware to parse JSON bodies
 app.use(express.json());
